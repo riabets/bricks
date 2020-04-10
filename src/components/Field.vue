373310cf9f459d9e7f1aa30433afdc333e45c175
Marina Riabets <riabets.mv@gmail.com>
2020-04-10 21:43:20 +0300

    Add colours buttons

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -1,5 +1,16 @@
 <template>
     <main>
+        <header>
+            <div
+                class="colours"
+                v-for="(item, index) in colours"
+                v-bind:key="index"
+                v-bind:style="{ color: item.color === 'white' ? 'black' : item.color }"
+                v-on:click="chooseColour(item.color)"
+            >
+                {{item.color}}
+            </div>
+        </header>
         <div
             class="brick"
             v-for="(item, index) in bricks"
@@ -15,24 +26,50 @@ export default {
     name: "Field",
     data() {
         return {
-            bricks: []
+            bricks: [],
+            activeColor: 'yellowgreen',
+            colours: [
+                {
+                    id: 1,
+                    color: 'black',
+                },
+                {
+                    id: 2,
+                    color: 'red',
+                },
+                {
+                    id: 3,
+                    color: 'green',
+                },
+                {
+                    id: 4,
+                    color: 'white',
+                },
+                {
+                    id: 5,
+                    color: 'yellowgreen',
+                }
+            ]
         }
     },
-    created(){
-      this.generateBricks();
+    created() {
+        this.generateBricks();
     },
     methods: {
         generateBricks() {
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < 200; i++) {
                 let item = {
                     id: i,
-                    color: "white",
+                    color: 'white',
                 };
                 this.bricks.push(item);
             }
         },
-        hoverItems(index){
-            this.bricks[index].color = 'green';
+        hoverItems(index) {
+            this.bricks[index].color = this.activeColor;
+        },
+        chooseColour(color) {
+            this.activeColor = color;
         }
     }
 }
@@ -46,13 +83,35 @@ export default {
         display: block;
 
         .brick {
-            width: 50px;
-            height: 50px;
-            background-color: yellow;
+            width: 75px;
+            height: 75px;
             border: 2px solid yellowgreen;
             display: inline-block;
+
             &:hover {
-                cursor: url('../assets/brush.svg'), auto;
+                cursor: url('../assets/brush.png'), auto;
+            }
+        }
+
+        header {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            margin-bottom: 50px;
+
+            .colours {
+                background-color: white;
+                padding: 10px 20px;
+                border: 1px solid white;
+                border-radius: 4px;
+                margin-right: 20px;
+                text-transform: capitalize;
+                font-weight: bold;
+                font-family: Arial, sans-serif;
+
+                &:hover {
+                    cursor: pointer;
+                }
             }
         }
     }
